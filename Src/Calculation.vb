b95d0eca7923d4617910f629cbe871792f3ad531
Subhra Maiti <subhramaiti0@gmail.com>
2017-01-17 14:45:34 +0530

    Add files via upload

---
+++ Src/Calculation.vb
@@ -0,0 +1,67 @@
+﻿Imports Pot_Bearing_Design_Calculation.DesignOutput
+Imports System.Diagnostics
+
+Module Calculation
+    Public Function calculate(expression As Double, Optional roundoffagainst As Double = Nothing, Optional minimumvalue As Double = Nothing)
+
+        If (roundoffagainst <> Nothing) Then
+            expression = Math.Ceiling(expression / roundoffagainst) * roundoffagainst
+        End If
+
+        If (minimumvalue <> Nothing) Then
+            If expression < minimumvalue Then
+                expression = minimumvalue
+            End If
+        End If
+
+        Return New DesignOutput(expression)
+
+    End Function
+
+    Public Sub checkSafeOrFail(expression As Double, comparisonoperator As String, comparedValue As Double)
+        Dim stackTrace As StackTrace = New StackTrace
+        Dim parent_method_name As String = stackTrace.GetFrame(1).GetMethod().Name.ToString()
+
+        If comparisonoperator = "Greater Than" Then
+            If expression <= comparedValue Then
+                checkSafeOrFailMessage(parent_method_name, "is below permissible limit.")
+            End If
+        End If
+
+        If comparisonoperator = "Less Than" Then
+            If expression >= comparedValue Then
+                checkSafeOrFailMessage(parent_method_name, "is above permissible limit.")
+            End If
+        End If
+
+    End Sub
+
+    Private Sub checkSafeOrFailMessage(methodName As String, message As String)
+        methodName = methodName.Replace("check", "")
+        Dim messagetoshow As String = ""
+        Dim capitalLetterCount As Integer = 0
+
+        For Each c As Char In methodName
+            Dim charCode As Integer = AscW(c)
+            If charCode >= 65 AndAlso charCode < 91 Then
+                capitalLetterCount += 1
+
+                If capitalLetterCount <> 1 Then
+                    messagetoshow = messagetoshow + " " + c.ToString.ToLower
+                Else
+                    messagetoshow = messagetoshow + c.ToString
+                End If
+            Else
+                messagetoshow = messagetoshow + c.ToString
+            End If
+        Next
+
+        MsgBox(messagetoshow + " " + message)
+    End Sub
+
+
+    Public Sub checkSafeOrAdjust()
+
+    End Sub
+
+End Module
---
+++ Src/Part.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Reflection
+
+Public MustInherit Class Part
+    Protected _input_parameters As Dictionary(Of String, Object)
+
+    Protected Sub extractInputs()
+        Dim fields As FieldInfo() = Me.GetType().GetRuntimeFields
+
+        For Each field As FieldInfo In fields
+            If field.FieldType = GetType(DesignInput) Then
+                findInputParameterAndSetValue(field)
+            End If
+        Next
+
+    End Sub
+
+    Protected MustOverride Sub calculateProperties()
+
+    Public Function generateOutput()
+        Dim fields As FieldInfo() = Me.GetType().GetRuntimeFields
+        For Each field As FieldInfo In fields
+            If field.FieldType = GetType(DesignOutput) Then
+                addToInputParameters(field)
+            End If
+        Next
+
+        Return _input_parameters
+    End Function
+
+    Private Sub findInputParameterAndSetValue(field As FieldInfo)
+        Dim _list_of_input_parameters_name As New List(Of String)(_input_parameters.Keys)
+
+        For Each _input_parameter_name As String In _list_of_input_parameters_name
+            If _input_parameter_name = field.Name Then
+                field.SetValue(Me, New DesignInput(_input_parameters(_input_parameter_name)))
+            End If
+        Next
+    End Sub
+
+    Private Sub addToInputParameters(field As FieldInfo)
+        _input_parameters.Add(field.Name, field.GetValue(Me).value)
+    End Sub
+End Class
+
+
